ee0199eafe49325d9d7c0383e77e0f686121c816
talialac <talia.lacombe@gmail.com>
2019-10-27 00:47:18 +0200

    Tests passed when numbers are negative in Money class

--- src/main/java/lacombedulionvert/Money.java
+++ src/main/java/lacombedulionvert/Money.java
@@ -4,6 +4,14 @@ public class Money {
     public static int value;
 
     public Money(int value) {
-        this.value = value;
+            this.value = Math.abs(value);
+    }
+
+    public static int getValue() {
+        return value;
+    }
+
+    public static void setValue(int value) {
+        Money.value = Math.abs(value);
     }
 }
--- src/test/java/lacombedulionvert/MoneyTest.java
+++ src/test/java/lacombedulionvert/MoneyTest.java
@@ -8,6 +8,26 @@ public class MoneyTest {
     @Test
     public void money_if_it_is_a_positive_number() {
         Money money_50 = new Money(50);
-        assertEquals(50, Money.value);
+        assertEquals(50, Money.getValue());
+    }
+
+    @Test
+    public void money_if_it_is_a_negative_number() {
+        Money money_50 = new Money(-50);
+        assertEquals(50, Money.getValue());
+    }
+
+    @Test
+    public void change_money_value() {
+        Money money_50 = new Money(50);
+        Money.setValue(80);
+        assertEquals(80, Money.getValue());
+    }
+
+    @Test
+    public void change_money_with_neg_value_should_ignore_negative() {
+        Money money_50 = new Money(50);
+        Money.setValue(-80);
+        assertEquals(80, Money.getValue());
     }
 }
