60698ac16c54cb81300147a8581276c3b6ac33c2
talialac <talia.lacombe@gmail.com>
2019-10-27 14:22:47 +0100

    Test a_withdrawal_operation passed

--- src/test/java/lacombedulionvert/OperationTest.java
+++ src/test/java/lacombedulionvert/OperationTest.java
@@ -12,4 +12,12 @@ public class OperationTest {
         int amount_of_deposit = deposit.getAmount();
         assertEquals(50, deposit.getAmount());
     }
+
+    @Test
+    public void a_withdrawal_operation() {
+        Money money_50 = new Money(50);
+        Operation withdrawal = new Operation(money_50);
+        int amount_of_deposit = withdrawal.getAmount();
+        assertEquals(50, withdrawal.getAmount());
+    }
 }
