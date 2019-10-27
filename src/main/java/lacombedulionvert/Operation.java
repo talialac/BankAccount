e8fb04cab1e79bea3315bb5b4b9463dad2270500
talialac <talia.lacombe@gmail.com>
2019-10-27 18:56:58 +0100

    Test create_should_have_correct_data_when_building_date_operation passed

--- src/main/java/lacombedulionvert/Operation.java
+++ src/main/java/lacombedulionvert/Operation.java
@@ -1,13 +1,31 @@
 package lacombedulionvert;
 
+import java.time.LocalDateTime;
+import java.util.Objects;
+
 public class Operation {
     private Money amount;
+    LocalDateTime date_time;
 
     Operation(Money amount) {
         this.amount = amount;
     }
 
+    public Operation(Money money_50, LocalDateTime date_time) {
+        this.amount = money_50;
+        this.date_time = date_time;
+    }
+
     public int getAmount() {
         return amount.getValue();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Operation that = (Operation) o;
+        return Objects.equals(amount, that.amount) &&
+                Objects.equals(date_time, that.date_time);
     }
 }
